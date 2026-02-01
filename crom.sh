476c86197119107842c2dfdcf7cfed167d1e2729
Trevonn <Trevonn@users.noreply.github.com>
2026-02-01 19:10:29 +0000

    Check for binary in /usr/bin before sourcing function

--- crom.sh
+++ crom.sh
@@ -2,27 +2,29 @@
 #
 # Functions for compressing game roms
 
-# Compress a single rom file
-# $1 dvd or cd
-# $2 source file
-crom() {
-    chdman create$1 -f -i "$2" -o "${2%.*}.chd"
-}
+if [[ -f /usr/bin/chdman ]] then
+    # Compress a single rom file
+    # $1 dvd or cd
+    # $2 source file
+    crom() {
+        chdman create$1 -f -i "$2" -o "${2%.*}.chd"
+    }
 
-# Compresses multiple rom files at once
-# $1 source file
-crom_batch() {
-    case $1 in
-    "dvd")
-        find -type f -iname "*.iso" -or -iname "*.bin" | parallel chdman createdvd -f -i "{}" -o "{.}.chd"
-        ;;
-    "cd")
-        find -type f -iname "*.cue" | parallel chdman createcd -f -i "{}" -o "{.}.chd"
-        ;;
-    "7z")
-        find -type f -iname "*.$2" | parallel --bar 7z a -mx9 "{.}.7z" "{}"
-        ;;
-    *)
-        echo "Incorrect or no option chosen"
-    esac
-}
+    # Compresses multiple rom files at once
+    # $1 source file
+    crom_batch() {
+        case $1 in
+        "dvd")
+            find -type f -iname "*.iso" -or -iname "*.bin" | parallel chdman createdvd -f -i "{}" -o "{.}.chd"
+            ;;
+        "cd")
+            find -type f -iname "*.cue" | parallel chdman createcd -f -i "{}" -o "{.}.chd"
+            ;;
+        "7z")
+            find -type f -iname "*.$2" | parallel --bar 7z a -mx9 "{.}.7z" "{}"
+            ;;
+        *)
+            echo "Incorrect or no option chosen"
+        esac
+    }
+fi
