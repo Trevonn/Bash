b4c98ec252582d0fa02bab37a1f9b06e01123c5f
Trevonn <Trevonn@users.noreply.github.com>
2026-06-25 01:11:22 +0100

    Refactor download_proton functions

--- bash_functions.sh
+++ bash_functions.sh
@@ -270,43 +270,37 @@ github_download() {
 }
 
 download_proton() {
-    # $1 = folder name
-    # $2 = file name
-    # $3 = file extension
-    # $4 = repo name
-    local proton_file="/mnt/NAS/Temp/Proton/$1/$2-latest$3"
-
-    if [[ $HOSTNAME == 'nas' ]]; then
-        if [[ "$4" == "CachyOS/proton-cachyos" ]]; then
-            github_download "x86_64.tar.xz" "https://api.github.com/repos/$4/releases/latest"
-        else
-            github_download "$3" "https://api.github.com/repos/$4/releases/latest"
-        fi
-        mv "$2"*"$3" "$proton_file"
+    local jq_cmd="jq -r .assets.[].browser_download_url"
+    local tar_file="$1.$3"
+    local nas_dest="/mnt/NAS/Temp/Proton/$1/$tar_file"
+    if [[ $HOSTNAME == "nas" ]]; then
+        wcurl --curl-options="--clobber --continue-at -" -o "$nas_dest" "$(curl -s $2 | $jq_cmd | $4)"
     else
-        local dest="$XDG_DATA_HOME/Steam/compatibilitytools.d/$2-latest"
-        rm -rf "$dest"
-        scp "$USER@nas:$proton_file" "$PWD"
-        mkdir "$dest"
-        tar -xf "$2-latest$3" -C "$dest" --strip-components 1
-        rm ./"$2"*"$3"
+        local dest="$XDG_DATA_HOME/Steam/compatibilitytools.d/$1-latest"
+        if [[ -d "$dest" ]] then
+            sudo rm -r "$dest"
+        fi
+        scp "$USER@nas:$nas_dest" "$PWD"
+        tar -xf "$tar_file" --one-top-level="$dest" --strip-components 1
+        rm "$tar_file"
+        ln -s "$HOME/Sync/Config/Gaming/Proton/$1/$HOSTNAME/user_settings.py" "$dest"/user_settings.py
     fi
 }
 
 download_protonge() {
-    download_proton proton-ge GE-Proton .tar.gz GloriousEggroll/proton-ge-custom
-    if [[ $HOSTNAME != 'nas' ]]; then
-        local dest="$XDG_DATA_HOME/Steam/compatibilitytools.d/GE-Proton-latest"
-        ln -s "$HOME/Sync/Config/Gaming/Proton/$HOSTNAME/user_settings.py" "$dest"/user_settings.py
-    fi
+    local proton_rel="proton-ge"
+    local github_url="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
+    local file_ext="tar.gz"
+    local grep_cmd="grep -v -e aarch64 -e sha512sum"
+    download_proton "$proton_rel" "$github_url" "$file_ext" "$grep_cmd"
 }
 
 download_protoncachyos() {
-    download_proton proton-cachyos proton-cachyos .tar.xz CachyOS/proton-cachyos
-    if [[ $HOSTNAME != 'nas' ]]; then
-        local dest="$XDG_DATA_HOME/Steam/compatibilitytools.d/proton-cachyos-latest"
-        ln -s "$HOME/Sync/Config/Gaming/Proton/proton-cachyos/user_settings.py"  "$dest"/user_settings.py
-    fi
+    local proton_rel="proton-cachyos"
+    local github_url="https://api.github.com/repos/CachyOS/proton-cachyos/releases/latest"
+    local file_ext="tar.xz"
+    local grep_cmd="grep -v -e arm64 -e sha512sum -e x86_64_v3"
+    download_proton "$proton_rel" "$github_url" "$file_ext" "$grep_cmd"
 }
 
 # Gaming - GPU
